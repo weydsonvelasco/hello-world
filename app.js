ea246a338ea110868bd21f9b7fb3224a43e249ae
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 09:09:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-systemctl cat dhcpd
+ systemctl restart dhcpd
+^C
+mv: cannot stat '/tmp/.dhcpd.conf.c7290c781dbd6f219c60d91502939f54': No such file or directory
+[root@cadclcldlx010 ~]# systemctl restart dhcpd
+[root@cadclcldlx010 ~]# systemctl cat dhcpd
 # /usr/lib/systemd/system/dhcpd.service
 [Unit]
 Description=DHCPv4 Server Daemon
@@ -17,4 +21,10 @@ StandardError=null
 WantedBy=multi-user.target
 [root@cadclcldlx010 ~]# type systemctl
 systemctl is hashed (/usr/bin/systemctl)
-[root@cadclcldlx010 ~]#
+[root@cadclcldlx010 ~]# witch systemctl
+-bash: witch: command not found
+[root@cadclcldlx010 ~]# which systemctl
+/usr/bin/systemctl
+[root@cadclcldlx010 ~]# rpm -qf $(which systemctl)
+systemd-252-46.el9_5.2.x86_64
+[root@cadclcldlx010 ~]
