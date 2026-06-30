77a18583a4c22021a58a1e8da3ec863509d8e31c
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 10:01:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-2026-06-30T12:58:41.2900882Z OPENSHIFT NPRD LOTERIAS - des, tqs ou hmp
-2026-06-30T12:58:41.4359706Z oc v3.11.0+0cbc58b
-2026-06-30T12:58:41.4360267Z kubernetes v1.11.0+d4cacc0
-2026-06-30T12:58:41.4360590Z features: Basic-Auth GSSAPI Kerberos SPNEGO
-2026-06-30T12:58:41.4514772Z 
-2026-06-30T12:58:41.4548117Z Server https://api.nctvmrh001.nuvem.caixa:6443
-2026-06-30T12:58:41.4548331Z kubernetes v1.33.12
-2026-06-30T12:58:41.7203265Z Iniciando criação da APP
-2026-06-30T12:58:41.8840270Z error: unable to process template
-2026-06-30T12:58:41.8877043Z   processedtemplates.template.openshift.io is forbidden: User "system:serviceaccount:default:svc-ads" cannot create resource "processedtemplates" in API group "template.openshift.io" in the namespace "openshift"
-2026-06-30T12:58:41.8877257Z error: no objects passed to create
+oc auth can-i create processedtemplates.template.openshift.io \
+  -n openshift \
+  --as=system:serviceaccount:default:svc-ads
+
+
+- apiGroups:
+  - security.openshift.io
+  resources:
+  - securitycontextconstraints
+  verbs:
+  - get
+  - list
+  - watch
