2904ee74706e71132dde599e21890d5f088a4498
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 10:16:44 -0300

    Add commands for updating security context constraints
    Added commands to patch cluster role and check permissions for updating security context constraints.

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-2026-06-30T13:14:41.9896630Z Essa aplicação já está criada.
-2026-06-30T13:14:42.4093387Z deployment.apps/sample-quarkus-ocp-des paused
-2026-06-30T13:14:42.6541440Z deployment.apps/sample-quarkus-ocp-des patched
-2026-06-30T13:14:42.8198932Z Error from server (Forbidden): securitycontextconstraints.security.openshift.io "anyuid" is forbidden: User "system:serviceaccount:default:svc-ads" cannot update resource "securitycontextconstraints" in API group "security.openshift.io" at the cluster scope
+oc patch clusterrole devops-deployer --type='json' -p='[
+  {
+    "op": "add",
+    "path": "/rules/-",
+    "value": {
+      "apiGroups": ["security.openshift.io"],
+      "resources": ["securitycontextconstraints"],
+      "verbs": ["get","list","watch","update","patch"]
+    }
+  }
+]'
+
+oc auth can-i update securitycontextconstraints \
+  --as=system:serviceaccount:default:svc-ads
