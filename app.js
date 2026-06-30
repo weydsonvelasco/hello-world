77275127422843638e76451b0a2c1b386607cf1b
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 10:50:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-voltou a dar aquele 
- 
-'2026-06-30T13:46:37.2826256Z deployment.apps/sample-quarkus-ocp-des patched (no change)
-2026-06-30T13:46:37.3630494Z Error from server (Forbidden): clusterrolebindings.rbac.authorization.k8s.io "system:openshift:scc:anyuid" is forbidden: User "system:serviceaccount:default:svc-ads" cannot get resource "clusterrolebindings" in API group "rbac.authorization.k8s.io" at the cluster scope'
- 
+oc patch clusterrole devops-deployer --type='json' -p='[
+  {
+    "op":"add",
+    "path":"/rules/-",
+    "value":{
+      "apiGroups":["rbac.authorization.k8s.io"],
+      "resources":["clusterrolebindings"],
+      "verbs":["get","list","watch","create","update","patch"]
+    }
+  }
+]'
