5c26f68ff966712c46a4c15b2729f2b24b79e030
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 09:12:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
- systemctl restart dhcpd
-^C
-mv: cannot stat '/tmp/.dhcpd.conf.c7290c781dbd6f219c60d91502939f54': No such file or directory
-[root@cadclcldlx010 ~]# systemctl restart dhcpd
-[root@cadclcldlx010 ~]# systemctl cat dhcpd
-# /usr/lib/systemd/system/dhcpd.service
-[Unit]
-Description=DHCPv4 Server Daemon
-Documentation=man:dhcpd(8) man:dhcpd.conf(5)
-Wants=network-online.target
-After=network-online.target
-After=time-sync.target
+echo "$PROMPT_COMMAND"
 
-[Service]
-Type=notify
-EnvironmentFile=-/etc/sysconfig/dhcpd
-ExecStart=/usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf -user dhcpd -group dhcpd --no-pid $DHCPDARGS
-StandardError=null
+trap -p
 
-[Install]
-WantedBy=multi-user.target
-[root@cadclcldlx010 ~]# type systemctl
-systemctl is hashed (/usr/bin/systemctl)
-[root@cadclcldlx010 ~]# witch systemctl
--bash: witch: command not found
-[root@cadclcldlx010 ~]# which systemctl
-/usr/bin/systemctl
-[root@cadclcldlx010 ~]# rpm -qf $(which systemctl)
-systemd-252-46.el9_5.2.x86_64
-[root@cadclcldlx010 ~]
+alias
+
+declare -F
+
+type mv
+
+type vim
