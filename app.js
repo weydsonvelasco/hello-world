107e1d2d618323897db9cbed9a2ea21556a50cbf
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 08:44:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 ^C
 mv: cannot stat '/tmp/.named.conf.ea9c9414940d1807afa19876a6961199': No such file or directory
 [root@cadclcldlx011 ~]#
+
+systemctl restart dhcpd
+^C
+mv: cannot stat '/tmp/.dhcpd.conf.4c48836041167720e11e8e38edda0465': No such file or directory
