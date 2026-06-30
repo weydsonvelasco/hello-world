0b616112d44fd38b28e13add0eb22f11c3629b34
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 10:15:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-oc patch clusterrole devops-deployer --type=json -p='[
-  {
-    "op":"add",
-    "path":"/rules/-",
-    "value":{
-      "apiGroups":["template.openshift.io"],
-      "resources":["templates","processedtemplates"],
-      "verbs":["*"]
-    }
-  }
-]'
+2026-06-30T13:14:41.9896630Z Essa aplicação já está criada.
+2026-06-30T13:14:42.4093387Z deployment.apps/sample-quarkus-ocp-des paused
+2026-06-30T13:14:42.6541440Z deployment.apps/sample-quarkus-ocp-des patched
+2026-06-30T13:14:42.8198932Z Error from server (Forbidden): securitycontextconstraints.security.openshift.io "anyuid" is forbidden: User "system:serviceaccount:default:svc-ads" cannot update resource "securitycontextconstraints" in API group "security.openshift.io" at the cluster scope
