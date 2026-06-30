3ea16aff05006fe049ab96310479548ef9388c6c
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 10:08:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-oc auth can-i create processedtemplates.template.openshift.io \
-  -n openshift \
-  --as=system:serviceaccount:default:svc-ads
-
-
-- apiGroups:
-  - security.openshift.io
-  resources:
-  - securitycontextconstraints
-  verbs:
-  - get
-  - list
-  - watch
-
-
-- apiGroups:
-  - template.openshift.io
-  resources:
-  - templates
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update
-  - patch
-  - delete
+[root@cadclcldlx012 ~]# oc auth can-i create processedtemplates.template.openshift.io   -n openshift   --as=system:serviceaccount:default:svc-ads
+no
+[root@cadcl
