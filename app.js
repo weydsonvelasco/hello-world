68d457e64bfdd2c2f1f8445c766f975953789451
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-06-30 09:28:50 -0300

    Refactor app.js and add ClusterRole for devops-deployer
    Removed unnecessary shell commands and added ClusterRole for devops-deployer with permissions for various resources.

--- app.js
+++ app.js
@@ -1,169 +1,137 @@
-echo "$PROMPT_COMMAND"
+bom dia !!
+ 
+2026-06-30T12:14:49.2403916Z Error from server (Forbidden): projects.project.openshift.io "sample-des" is forbidden: User "system:serviceaccount:default:svc-ads" cannot get resource "projects" in API group "project.openshift.io" in the namespace "sample-des"
+2026-06-30T12:14:49.4311074Z Error from server (Forbidden): You may not request a new project via this API.
+2026-06-30T12:14:49.5900606Z Error from server (Forbidden): securitycontextconstraints.security.openshift.io "anyuid" is forbidden: User "system:serviceaccount:default:svc-ads" cannot get resource "securitycontextconstraints" in API group "security.openshift.io" at the cluster scope
+2026-06-30T12:14:49.8143382Z Error from server (Forbidden): namespaces "sample-des" is forbidden: User "system:serviceaccount:default:svc-ads" cannot get resource "namespaces" in API group "" in the namespace "sample-des"
+2026-06-30T12:14:50.0293918Z Error from server (Forbidden): namespaces "sample-des" is forbidden: User "system:serviceaccount:default:svc-ads" cannot get resource "namespaces" in API group "" in the namespace "sample-des"
+2026-06-30T12:14:50.2398943Z Error from server (Forbidden): namespaces "sample-des" is forbidden: User "system:serviceaccount:default:svc-ads" cannot get resource "namespaces" in API group "" in the namespace "sample-des"
+2026-06-30T12:14:51.9182581Z error: the server doesn't have a resource type "netnamespace"
 
-trap -p
 
-alias
-
-declare -F
-
-type mv
-
-type vim
-printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"
-trap -- 'loga' DEBUG
-alias cp='cp -i'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias grep='grep --color=auto'
-alias l.='ls -d .* --color=auto'
-alias ll='ls -l --color=auto'
-alias ls='ls --color=auto'
-alias mc='. /usr/libexec/mc/mc-wrapper.sh'
-alias mv='mv -i'
-alias rm='rm -i'
-alias xzegrep='xzegrep --color=auto'
-alias xzfgrep='xzfgrep --color=auto'
-alias xzgrep='xzgrep --color=auto'
-alias zegrep='zegrep --color=auto'
-alias zfgrep='zfgrep --color=auto'
-alias zgrep='zgrep --color=auto'
-declare -f __azcmagent_contains_word
-declare -f __azcmagent_debug
-declare -f __azcmagent_handle_command
-declare -f __azcmagent_handle_filename_extension_flag
-declare -f __azcmagent_handle_flag
-declare -f __azcmagent_handle_go_custom_completion
-declare -f __azcmagent_handle_noun
-declare -f __azcmagent_handle_reply
-declare -f __azcmagent_handle_subdirs_in_dir_flag
-declare -f __azcmagent_handle_word
-declare -f __azcmagent_index_of_word
-declare -f __azcmagent_init_completion
-declare -f __expand_tilde_by_ref
-declare -f __get_cword_at_cursor_by_ref
-declare -f __load_completion
-declare -f __ltrim_colon_completions
-declare -f __parse_options
-declare -f __reassemble_comp_words_by_ref
-declare -f __start_azcmagent
-declare -f _allowed_groups
-declare -f _allowed_users
-declare -f _authselect_completions
-declare -f _available_interfaces
-declare -f _azcmagent_check
-declare -f _azcmagent_config
-declare -f _azcmagent_config_clear
-declare -f _azcmagent_config_get
-declare -f _azcmagent_config_info
-declare -f _azcmagent_config_list
-declare -f _azcmagent_config_set
-declare -f _azcmagent_connect
-declare -f _azcmagent_disconnect
-declare -f _azcmagent_extension
-declare -f _azcmagent_extension_list
-declare -f _azcmagent_extension_remove
-declare -f _azcmagent_genkey
-declare -f _azcmagent_help
-declare -f _azcmagent_license
-declare -f _azcmagent_logs
-declare -f _azcmagent_root_command
-declare -f _azcmagent_show
-declare -f _azcmagent_version
-declare -f _bashcomp_try_faketty
-declare -f _bpftool
-declare -f _bpftool_get_btf_ids
-declare -f _bpftool_get_link_ids
-declare -f _bpftool_get_map_ids
-declare -f _bpftool_get_map_ids_for_type
-declare -f _bpftool_get_map_names
-declare -f _bpftool_get_map_names_for_type
-declare -f _bpftool_get_obj_map_idxs
-declare -f _bpftool_get_obj_map_names
-declare -f _bpftool_get_prog_ids
-declare -f _bpftool_get_prog_names
-declare -f _bpftool_get_prog_tags
-declare -f _bpftool_map_guess_map_type
-declare -f _bpftool_map_update_get_id
-declare -f _bpftool_map_update_get_name
-declare -f _bpftool_once_attr
-declare -f _bpftool_one_of_list
-declare -f _bpftool_search_list
-declare -f _cd
-declare -f _cd_devices
-declare -f _command
-declare -f _command_offset
-declare -f _comp_iprconfig
-declare -f _complete_as_root
-declare -f _completion_loader
-declare -f _configured_interfaces
-declare -f _count_args
-declare -f _dvd_devices
-declare -f _expand
-declare -f _filedir
-declare -f _filedir_xspec
-declare -f _fstypes
-declare -f _get_comp_words_by_ref
-declare -f _get_cword
-declare -f _get_first_arg
-declare -f _get_pword
-declare -f _gids
-declare -f _have
-declare -f _included_ssh_config_files
-declare -f _init_completion
-declare -f _installed_modules
-declare -f _ip_addresses
-declare -f _kernel_versions
-declare -f _known_hosts
-declare -f _known_hosts_real
-declare -f _longopt
-declare -f _mac_addresses
-declare -f _minimal
-declare -f _modules
-declare -f _ncpus
-declare -f _oscap
-declare -f _oscap_noarg
-declare -f _parse_help
-declare -f _parse_usage
-declare -f _pci_ids
-declare -f _pgids
-declare -f _pids
-declare -f _pnames
-declare -f _quote_readline_by_ref
-declare -f _realcommand
-declare -f _rl_enabled
-declare -f _root_command
-declare -f _service
-declare -f _services
-declare -f _shells
-declare -f _signals
-declare -f _split_longopt
-declare -f _strace
-declare -f _sysfs_get_netdevs
-declare -f _sysvdirs
-declare -f _terms
-declare -f _tilde
-declare -f _uids
-declare -f _upvar
-declare -f _upvars
-declare -f _usb_ids
-declare -f _user_at_host
-declare -f _usergroup
-declare -f _userland
-declare -f _variables
-declare -f _vdostats
-declare -f _xfunc
-declare -f _xinetd_services
-declare -f dequote
-declare -f gawklibpath_append
-declare -f gawklibpath_default
-declare -f gawklibpath_prepend
-declare -f gawkpath_append
-declare -f gawkpath_default
-declare -f gawkpath_prepend
-declare -f loga
-declare -f quote
-declare -f quote_readline
-declare -fx which
-mv is aliased to `mv -i'
-vim is hashed (/usr/bin/vim)
-[root@cadclcldlx010 ~]#
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: devops-deployer
+  uid: 4f5ee41b-da2f-48d8-9686-3f16af2b3610
+  resourceVersion: '4737210'
+  creationTimestamp: '2026-06-29T14:55:15Z'
+  managedFields:
+    - manager: Mozilla
+      operation: Update
+      apiVersion: rbac.authorization.k8s.io/v1
+      time: '2026-06-29T14:55:15Z'
+      fieldsType: FieldsV1
+      fieldsV1:
+        'f:rules': {}
+rules:
+  - verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+    apiGroups:
+      - ''
+    resources:
+      - configmaps
+      - secrets
+      - services
+      - serviceaccounts
+      - persistentvolumeclaims
+      - pods
+      - pods/log
+      - events
+  - verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+    apiGroups:
+      - apps
+    resources:
+      - deployments
+      - replicasets
+      - statefulsets
+      - daemonsets
+  - verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+    apiGroups:
+      - batch
+    resources:
+      - jobs
+      - cronjobs
+  - verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+    apiGroups:
+      - autoscaling
+    resources:
+      - horizontalpodautoscalers
+  - verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+    apiGroups:
+      - networking.k8s.io
+    resources:
+      - ingresses
+      - networkpolicies
+  - verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+    apiGroups:
+      - route.openshift.io
+    resources:
+      - routes
+  - verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+    apiGroups:
+      - image.openshift.io
+    resources:
+      - imagestreams
+      - imagestreamtags
+  - verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+    apiGroups:
+      - build.openshift.io
+    resources:
+      - builds
+      - buildconfigs
