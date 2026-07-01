27aa4fc742fbc0a75f0eab210a341eff2d9fae76
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-07-01 10:27:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-oc patch clusterrole devops-deployer --type='json' -p='[
-  {
-    "op":"add",
-    "path":"/rules/-",
-    "value":{
-      "apiGroups":["rbac.authorization.k8s.io"],
-      "resources":["clusterrolebindings"],
-      "verbs":["get","list","watch","create","update","patch"]
-    }
-  }
-]'
+2026-07-01T13:25:30.7517178Z Error from server (Forbidden): clusterrolebindings.rbac.authorization.k8s.io "system:openshift:scc:anyuid" is forbidden: user "system:serviceaccount:default:svc-ads" (groups=["system:serviceaccounts" "system:serviceaccounts:default" "system:authenticated"]) is attempting to grant RBAC permissions not currently held:
+2026-07-01T13:25:30.7517609Z {APIGroups:["security.openshift.io"], Resources:["securitycontextconstraints"], ResourceNames:["anyuid"], Verbs:["use"]}
+ 
