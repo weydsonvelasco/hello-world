03bec998ddfeb95d1b508c3f2ca8fe44059384ac
weydsonvelasco <97919190+weydsonvelasco@users.noreply.github.com>
2026-07-01 10:52:38 -0300

    Update app.js with RBAC patch command
    Add patch command to grant permissions for security context constraints.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-2026-07-01T13:25:30.7517178Z Error from server (Forbidden): clusterrolebindings.rbac.authorization.k8s.io "system:openshift:scc:anyuid" is forbidden: user "system:serviceaccount:default:svc-ads" (groups=["system:serviceaccounts" "system:serviceaccounts:default" "system:authenticated"]) is attempting to grant RBAC permissions not currently held:
-2026-07-01T13:25:30.7517609Z {APIGroups:["security.openshift.io"], Resources:["securitycontextconstraints"], ResourceNames:["anyuid"], Verbs:["use"]}
- 
+oc patch clusterrole devops-deployer --type='json' -p='[
+  {
+    "op":"add",
+    "path":"/rules/-",
+    "value":{
+      "apiGroups":["security.openshift.io"],
+      "resources":["securitycontextconstraints"],
+      "resourceNames":["anyuid"],
+      "verbs":["use"]
+    }
+  }
+]'
+
+
+
+
+
+
+oc auth can-i use securitycontextconstraints/anyuid \
+  --as=system:serviceaccount:default:svc-ads
